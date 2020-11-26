cb3d8871266308fd2c137f538ae5696d538eabb2
RANA AHMAD <74317544+RanaAhmad112@users.noreply.github.com>
2020-11-26 06:14:55 -0800

    I done lecture No 57 and design Male Icons inCardWidget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -16,16 +17,35 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: RepeatContainercode(colors: Color(0xFF1D1E33),
+              Expanded(child:new RepeatContainercode(
+                colors: Color(0xFF1D1E33),
+                cardwidget: Column(
+                  children: <Widget>[
+                    Icon(
+                      FontAwesomeIcons.male,
+                      size: 80.0,
+                    ),
+                    SizedBox(
+                      height: 80.0,
+                    ),
+                    Text(
+                      'MALE',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        color: Color(0xFF8D8E98),
+                      ),
+                    ),
+                  ],
+                ),
               ),),
-              Expanded(child:  RepeatContainercode(colors: Color(0xFF1D1E33),),),
+              Expanded(child:new  RepeatContainercode(colors: Color(0xFF1D1E33),),),
             ]
           ),),
           Expanded(child: RepeatContainercode(colors: Color(0xFF1D1E33),),),
           Expanded(child: Row(
               children: <Widget>[
-                Expanded(child:  RepeatContainercode(colors: Color(0xFF1D1E33),),),
-                Expanded(child:  RepeatContainercode(colors: Color(0xFF1D1E33),),),
+                Expanded(child:new  RepeatContainercode(colors: Color(0xFF1D1E33),),),
+                Expanded(child:new  RepeatContainercode(colors: Color(0xFF1D1E33),),),
               ]
           ),),
         ]
@@ -35,11 +55,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RepeatContainercode extends StatelessWidget {
- RepeatContainercode({@required this.colors});Color colors,
+ RepeatContainercode({@required this.colors,this.cardwidget});
+ final Color colors;
+ final Widget cardwidget;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardwidget,
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
