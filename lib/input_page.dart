478d656baea9cec0c919efda99d3ef343280c1a3
RANA AHMAD <74317544+RanaAhmad112@users.noreply.github.com>
2020-11-26 05:18:22 -0800

    itself change color of container(lec#57)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,15 +16,16 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: RepeatContainercode(),),
-              Expanded(child:  RepeatContainercode(),),
+              Expanded(child: RepeatContainercode(colors: Color(0xFF1D1E33),
+              ),),
+              Expanded(child:  RepeatContainercode(colors: Color(0xFF1D1E33),),),
             ]
           ),),
-          Expanded(child: RepeatContainercode(),),
+          Expanded(child: RepeatContainercode(colors: Color(0xFF1D1E33),),),
           Expanded(child: Row(
               children: <Widget>[
-                Expanded(child:  RepeatContainercode(),),
-                Expanded(child:  RepeatContainercode(),),
+                Expanded(child:  RepeatContainercode(colors: Color(0xFF1D1E33),),),
+                Expanded(child:  RepeatContainercode(colors: Color(0xFF1D1E33),),),
               ]
           ),),
         ]
@@ -34,16 +35,13 @@ class _InputPageState extends State<InputPage> {
 }
 
 class RepeatContainercode extends StatelessWidget {
-  const RepeatContainercode({
-    Key key,
-  }) : super(key: key);
-
+ RepeatContainercode({@required this.colors});Color colors,
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colors,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
