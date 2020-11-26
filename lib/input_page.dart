f3b0d1a8899dc206625599da4e0b90afed821ca8
RANA AHMAD <74317544+RanaAhmad112@users.noreply.github.com>
2020-11-26 04:52:40 -0800

    refactor code(lec#57)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,48 +16,35 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-              ),),
+              Expanded(child: RepeatContainercode(),),
+              Expanded(child:  RepeatContainercode(),),
             ]
           ),),
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              color: Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-          ),),
+          Expanded(child: RepeatContainercode(),),
           Expanded(child: Row(
               children: <Widget>[
-                Expanded(child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),),
-                Expanded(child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                ),),
+                Expanded(child:  RepeatContainercode(),),
+                Expanded(child:  RepeatContainercode(),),
               ]
           ),),
         ]
+      ),
+    );
+  }
+}
+
+class RepeatContainercode extends StatelessWidget {
+  const RepeatContainercode({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
